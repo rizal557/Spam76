903bd2a31440964000e10c76c68855ba22f8b6d9
RISKY-RIAU <76860656+Dumai-991@users.noreply.github.com>
2021-01-31 13:31:35 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+#Compile By Mr.Risky
+#Sudah Tidam Bisa Lagi DiUpcompile
+#Jangan DiEdit Goblok Natik Error
+#Xnxx.com
+#github.com/Dumai-991
+
+z="
+";Vz='e';Cz='echo';TBz='.sh';PBz='[037';fz='bash';Dz=' "\0';Yz='git ';dz='php';Nz='slee';Hz=' Pen';Mz='..."';BBz='wget';Bz='r';Fz='32mM';vz='node';Zz='pyth';OBz='\033';Ez='33[0';hz='libc';Lz='37m.';nz=' mec';Wz='inst';SBz='sh M';lz='pip2';az='on p';HBz='p 1';xz='term';mz=' ins';Qz='upda';ABz='pi';NBz='esai';Kz='an\0';Oz='p 2';pz='ze';Iz='gins';oz='hani';QBz='m...';kz='ssl';wz='js';tz=' bs4';rz='uest';Tz='g up';iz='url';uz='jq';jz='open';Pz='pkg ';GBz='fuck';IBz='32mP';Uz='grad';ez='nano';Az='clea';qz=' req';CBz='nmap';sz='s';LBz='llan';Gz='ulai';gz='curl';cz='n2';JBz='engi';FBz='uck';KBz='nsta';RBz='.."';Jz='tall';yz='ux-a';DBz='pip ';MBz=' Sel';Xz='all ';bz='ytho';Sz='& pk';EBz='thef';Rz='te &';
+eval "$Az$Bz$z$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Ez$Lz$Mz$z$Nz$Oz$z$Pz$Qz$Rz$Sz$Tz$Uz$Vz$z$Pz$Wz$Xz$Yz$Zz$az$bz$cz$z$Pz$Wz$Xz$dz$z$Pz$Wz$Xz$ez$z$Pz$Wz$Xz$fz$z$Pz$Wz$Xz$gz$z$Pz$Wz$Xz$hz$iz$z$Pz$Wz$Xz$jz$kz$z$lz$mz$Jz$nz$oz$pz$z$lz$mz$Jz$qz$rz$sz$z$lz$mz$Jz$tz$z$Pz$Wz$Xz$uz$z$Pz$Wz$Xz$vz$wz$z$Pz$Wz$Xz$xz$yz$ABz$z$Pz$Wz$Xz$BBz$z$Pz$Wz$Xz$CBz$z$DBz$Wz$Xz$EBz$FBz$z$GBz$z$Nz$HBz$z$Cz$Dz$Ez$IBz$JBz$KBz$LBz$MBz$NBz$OBz$PBz$QBz$RBz$z$Nz$Oz$z$SBz$Gz$TBz"
+
